e172011cf6e0005eb38ebf4aec30942967fc1965
alialaba <alialaba079@gmail.com>
2021-05-26 10:33:33 -0700

    implemented functionality for adding a todo task

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TodoList-App
-keywords: insertAdjacentHTML
+keywords: insertAdjacentHTML, Date;
+
+ FUNCTIONALITY IMPLEMENTATION
+1. dateInfo function that display the date on the header .
+2. addTodo function that added the html using insertAdjacentHTML.
+3. addToDoAfterKeypress function that show todo/task text been input by the user on hitting the enter key;
--- script/index.js
+++ script/index.js
@@ -1,6 +1,6 @@
 const clearBtn = document.querySelector(".clear-btn");
 const date = document.getElementById("date");
-const list = document.getElementById("list");
+const lists = document.getElementById("list");
 const input = document.getElementById("input");
 
 
@@ -10,8 +10,33 @@ const dateInfo = () => {
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     const myDate = new Date();
     const now = [days[myDate.getDay()] + ", " + months[myDate.getMonth()] + " " + myDate.getDate()];
-    console.log(now);
     date.innerHTML = now
 
 }
 dateInfo()
+
+
+//addToDo
+const addToDo = (todo) => {
+    //using insertAdjacentHTML method that accept 2 parameter (position, text)
+    let htmlText = `
+                <li class="item">
+                        <i class="far fa-circle " job="complete"></i>
+                        <p class="text">${todo}</p>
+                        <i class="fas fa-trash-alt" job="delete"></i>
+                 </li>
+                `
+    let position = 'beforeend';
+    lists.insertAdjacentHTML(position, htmlText);
+}
+
+
+const addToDoAfterKeyPress = (e) => {
+    let todo = input.value;
+    if (todo.length > 0 && e.keyCode === 13) {
+        addToDo(todo);
+        //clear out / empty the initial text insert 
+        input.value = "";
+    }
+}
+input.addEventListener("keypress", addToDoAfterKeyPress)
