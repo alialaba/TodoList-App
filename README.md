ee3d743378c1d82b0efa11e00092081923efa003
alialaba <alialaba079@gmail.com>
2021-05-26 09:14:36 -0700

    html code for the project

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # TodoList-App
+keywords: insertAdjacentHTML
---
+++ script/index.js
@@ -0,0 +1,5 @@
+const clearBtn = document.querySelector(".clear-btn");
+const date = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
