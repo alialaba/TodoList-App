83738ce065b904c1d79c9c62845006d65f1070fb
alialaba <alialaba079@gmail.com>
2021-05-26 10:02:32 -0700

    implement display of date

--- script/index.js
+++ script/index.js
@@ -3,3 +3,15 @@ const date = document.getElementById("date");
 const list = document.getElementById("list");
 const input = document.getElementById("input");
 
+
+//date function
+const dateInfo = () => {
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const myDate = new Date();
+    const now = [days[myDate.getDay()] + ", " + months[myDate.getMonth()] + " " + myDate.getDate()];
+    console.log(now);
+    date.innerHTML = now
+
+}
+dateInfo()
