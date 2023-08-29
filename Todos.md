1f374337fdc5129bf73568d54f9e17827f167ddd
alialaba <alialaba079@gmail.com>
2023-08-29 10:44:13 -0400

    ft: implement create task

---
+++ Todos.md
@@ -0,0 +1,11 @@
+- implement a submit form input value to create a todo
+- implement edit func
+- implement delete func
+- implement localstorage to storage todo
+
+
+
+keywords
+- window
+- addeventlistener
+- load
--- script/main.js
+++ script/main.js
@@ -1,10 +1,61 @@
+
 window.addEventListener("load", function(){
-    console.log("loaded successful");
+
+    //Select Element
+    const form = document.getElementById("todos__form");
+    const input = document.getElementById("todos__input");
+    const tasksEl = document.querySelector(".tasks");
+
+    form.addEventListener("submit", function(e){
+
+        //prevent form reload
+        e.preventDefault();
+
+        if(input.value !== ""){
+            //Create Elements
+            const taskEl = document.createElement("div");
+            const taskContent = document.createElement("div");
+            const taskActions = document.createElement("div");
+            const taskText = document.createElement("input");
+            const btnEdit = document.createElement("button");
+            const btnDelete = document.createElement("button");
+
+             //Set ClassNames
+            taskEl.classList.add("task");
+            taskContent.classList.add("task__content");
+            taskActions.classList.add("task__actions");
+            taskText.classList.add("task__text");
+            btnEdit.classList.add("task__edit");
+            btnDelete.classList.add("task__delete");
+
+            //get input value
+            const inputValue = input.value.trim();
+
+            //Set attributes
+            taskText.setAttribute("readonly", "readonly");
+
+            //Set Values
+            taskText.value = inputValue;
+            btnDelete.textContent = "Delete";
+            btnEdit.textContent ="Edit";
+
+            //Append Elements (according to tree stack);
+            taskContent.appendChild(taskText)
+            taskEl.appendChild(taskContent)
+            taskActions.appendChild(btnEdit)
+            taskActions.appendChild(btnDelete)
+            taskEl.appendChild(taskActions)
+            tasksEl.appendChild(taskEl)
+
+            //clear input field
+            input.value = ""
+
+
+        }  
+
+        
+    
+        
+    })
+
 })
-
-
-
-
-
-
-
